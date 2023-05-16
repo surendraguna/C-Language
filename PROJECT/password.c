5fc472cabd38bec7f64d7b9aa188e92845b48781
BOYA SURENDRA <125847917+surendra-guna@users.noreply.github.com>
2023-05-16 22:24:36 +0530

    Add files via upload

---
+++ PROJECT/password.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+bool verify(char *p)
+{
+	bool f = false;
+	if(strlen(p) > 6 && strlen(p) < 10)
+		f = true;
+	int i;
+	bool a = false, b = false, e = false, d = false;
+	for(i=0; i<strlen(p); i++)
+	{
+		char c = p[i];
+		if(c >= '0' && c <= '9')
+			a = true;
+		else if(c >= 'a' && c <= 'z')
+			b = true;
+		else if(c >= 'A' && c <= 'Z')
+			d = true;	
+		else
+			e = true;
+	}
+	return (f && a && b && d && e); 
+}
+void main()
+{
+	char *p;
+	gets(p);
+	if(verify(p))
+		printf("OK");
+	else
+		Beep(500,400);
+}
